c002a26be7384c2a3348a4e55ba0dcf4e4a71830
Maxi Ordoñez <exemaxi@outlook.com>
2025-06-16 19:59:00 -0300

    se termino el 03-tp-bucles

--- js/bucles.js/03-tp-bucles.js
+++ js/bucles.js/03-tp-bucles.js
@@ -5,3 +5,24 @@
 Nota: usar confirm() https://www.w3schools.com/jsref/met_win_confirm.asp
 
  */
+
+let textoFinal = "";       // Donde se van a ir guardando las cadenas
+let primera = true;        // Para no poner guión al principio
+
+while (true) {
+  let input = prompt("Escribí algo (hacé clic en 'Cancelar' para terminar):");
+
+  if (input === null) {
+    // El usuario tocó "Cancelar"
+    break;
+  }
+
+  if (primera) {
+    textoFinal += input;
+    primera = false;
+  } else {
+    textoFinal += "-" + input;
+  }
+}
+
+alert("Resultado final: " + textoFinal);
