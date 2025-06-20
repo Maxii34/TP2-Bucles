0c8487daa3abcbae587f43b8fef56470e0cdc43c
Maxi Ordoñez <exemaxi@outlook.com>
2025-06-20 11:42:05 -0300

    Se agrego el 12-tp-bucles

---
+++ js/bucles.js/12-tp-bucles.js
@@ -0,0 +1,7 @@
+//12- Realiza un script que genere un número aleatorio entre 1 y 99
+
+//generar numero del 1 al 99
+// Generar número aleatorio entre 1 y 99
+let numeroAleatorio = Math.floor(Math.random() * 99) + 1;
+
+alert(`El número aleatorio generado es: ${numeroAleatorio}`);
